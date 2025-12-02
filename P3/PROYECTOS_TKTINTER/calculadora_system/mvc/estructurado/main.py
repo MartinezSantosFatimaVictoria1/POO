9dadbfe2c2e05f7a7829d7c99e50eba0ab214a7d
MartinezSantosFatimaVictoria1 <fatimavictoriamartinez23@gmail.com>
2025-12-01 23:37:59 -0600

    Add files via upload

---
+++ P3/PROYECTOS_TKTINTER/calculadora_system/mvc/estructurado/main.py
@@ -0,0 +1,16 @@
+"""
+crear una calculadora:
+1.-dos botones de campo de texto
+2.- 4 botones para las operaciones
+3.-Mostrar el resultado en una alerta
+4.-Programacion Estructurada
+5.-Implementar el MVC
+"""
+
+from view import interfaz
+
+def main():
+    interfaz.interfaz()
+
+if __name__ =='__main__':
+    main()    
